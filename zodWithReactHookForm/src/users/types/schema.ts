a4a6c477999c3ebdc46c9d9f3efb0c507a299cfc
CODSensei <krishna2342005@gmail.com>
2025-04-04 15:44:33 +0530

    basics of zod and react-hook-form completed, mui started

---
+++ zodWithReactHookForm/src/users/types/schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { patterns } from "../../constants";
+
+export const schema = z.object({
+  name: z.string().min(1, {
+    message: "atleast 1 character",
+  }),
+  email: z
+    .string()
+    .min(1, {
+      message: "email is required",
+    })
+    .refine((email) => patterns.email.test(email), {
+      message: "email not valid",
+    }),
+});
